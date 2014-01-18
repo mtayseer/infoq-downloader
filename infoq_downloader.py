3eeecc03a3dfc1a70a6963dfda5cc27e1214a33d
Mosab Ahmad <mahmad@yubb-software.com>
2014-01-18 14:05:51 +0200

    Cleaned up paths to handle OS differences

--- infoq_downloader.py
+++ infoq_downloader.py
@@ -8,21 +8,27 @@ import requests
 import lxml.html
 
 
+# Some settings
+download_directory = 'downloads'
+cleanup_elements = ['#footer', '#header', '#topInfo', '.share_this', '.random_links', '.vendor_vs_popular',
+                    '.bottomContent', '#id_300x250_banner_top', '.presentation_type', '#conference', '#imgPreload',
+                    '#text_height_fix_box', '.download_presentation', '.recorded', 'script[async]']
+
 # Set argparse to parse the paramaters
 parser = argparse.ArgumentParser(description='Download InfoQ presentations.')
 parser.add_argument('url', metavar='URL', type=str, help='URL of the presentation to download')
 
-# read the paramaters
+# Parse the arguments passed to the script
 args = parser.parse_args()
-
-# Set the url
 url = args.url
 
 # Tell infoq that I'm an iPad, so it gives me simpler HTML to parse & mp4 file to download
 user_agent = "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 " \
              "(KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10')"
 
+# Start downloading
 print 'Downloading HTML file'
+
 content = requests.get(url, headers={'User-Agent': user_agent}).content
 html_doc = lxml.html.fromstring(content)
 title = html_doc.find(".//title").text
@@ -31,11 +37,8 @@ video_file = os.path.split(video_url)[1]
 html_doc.cssselect('video > source')[0].attrib['src'] = video_file
 
 # Clean the page
-for elt in html_doc.cssselect('#footer, #header, #topInfo, .share_this, .random_links,' +
-                              ' .vendor_vs_popular, .bottomContent, #id_300x250_banner_top,' +
-                              ' .presentation_type, #conference, #imgPreload, #text_height_fix_box, ' +
-                              '.download_presentation, .recorded, script[async]'):
-    elt.getparent().remove(elt)
+for elt in html_doc.cssselect():
+    elt.getparent(', '.join(e for e in cleanup_elements)).remove(elt)
 html_doc.cssselect('#wrapper')[0].attrib['style'] = 'background: none'
 content = lxml.html.tostring(html_doc)
 
@@ -43,33 +46,34 @@ content = lxml.html.tostring(html_doc)
 slides_re = re.compile(r"'(/resource/presentations/[^']*?/en/slides/[^']*?)'")
 slides = slides_re.findall(content)
 
-# Create a directory for the downloaded presentation
-download_directory = 'downloads'
+# Create a directory for the downloaded presentation if it doesn't exist
+if not os.path.exists(download_directory):
+    os.makedirs(download_directory)
 
-# If the downloads folder does not exist, create it
-if not os.path.exists('{}'.format(download_directory)):
-    os.makedirs('{}'.format(download_directory))
-
+# presentation folder path
+presentation_directory = os.path.join(download_directory, title)
 # Create a folder with the name of the presentation
-if not os.path.exists('{}/{}'.format(download_directory, title)):
-    os.makedirs('{}/{}'.format(download_directory, title))
+if not os.path.exists(presentation_directory):
+    os.makedirs(presentation_directory)
 
 # Create a slides folder inside the presentation folder
-if not os.path.exists('{}/{}/slides'.format(download_directory, title)):
-    os.makedirs('{}/{}/slides'.format(download_directory, title))
+if not os.path.exists('{}/slides'.format(presentation_directory)):
+    os.makedirs('{}/slides'.format(presentation_directory))
 
+#Write content
 content = re.sub(r"/resource/presentations/[^']*?/en/", '', content)
-with open('{}/{}/index.html'.format(download_directory, title), 'w') as f:
+with open('{}/index.html'.format(presentation_directory), 'w') as f:
     f.write(content)
     f.flush()
 
 for i, slide in enumerate(slides):
     filename = os.path.split(slide)[1]
-    if os.path.exists('{}/{}'.format(download_directory, title) + '/slides/{0}'.format(filename)):
+    if os.path.exists(os.path.join(presentation_directory, 'slides', '{0}'.format(filename))):
         continue
     print '\rDownloading slide {0} of {1}'.format(i, len(slides)),
     url = 'http://www.infoq.com{0}'.format(slide)
-    open('{}/{}'.format(download_directory, title) + '/slides/{0}'.format(filename), 'wb').write(requests.get(url).content)
+    with open(os.path.join(presentation_directory, 'slides', '{0}'.format(filename), 'wb')) as f:
+        f.write(requests.get(url).content)
 
 print
 
@@ -95,4 +99,4 @@ with open(downloaded_file, 'ab') as f:
         # The comma at the end of line is important, to stop the 'print' command from printing an additional new line
         print '\rDownloading video {0}%'.format(round(f.tell() / content_length, 2) * 100),
 
-os.rename(downloaded_file, '{}/{}/{}'.format(download_directory, title,video_file))
+os.rename(downloaded_file, '{}/{}/{}'.format(download_directory, title, video_file))
